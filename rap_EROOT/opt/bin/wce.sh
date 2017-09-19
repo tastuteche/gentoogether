fd3977bd8c2854490fce68ca12d36585ff4f1583
tastu teche <tastuteche@yahoo.com>
2017-09-19 15:26:48 +0800

    toggle emacs windowstate:minimized normal

--- rap_EROOT/opt/bin/wce.sh
+++ rap_EROOT/opt/bin/wce.sh
@@ -3,6 +3,7 @@
 
 
 pid=$(pgrep -af 'emacs --daemon'|awk '{print $1}')
+active_wid=$(xprop -root | grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}' | sed 's/,//' | sed 's/^0x/0x0/')
 if [[ "x$pid" == "x" ]]; then
     :
 else
@@ -10,7 +11,12 @@ else
     if [[ "x$wid" == "x" ]]; then
         :
     else
-        wmctrl -i -a "$wid"
+        if [[ "x$wid" == "x$active_wid" ]]; then
+            xdotool windowminimize $(xdotool getactivewindow)
+        else
+            wmctrl -i -a "$wid"
+        fi
+        
     fi
     
 fi
